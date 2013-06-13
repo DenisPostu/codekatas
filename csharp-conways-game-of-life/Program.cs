11caf23deaef8523961cd27061d6645aa07039ae
Denis Postu <dpostu@kno.com>
2013-06-13 10:28:49 +0300

    C# and F# solutions to Conway's Game of Life

---
+++ csharp-conways-game-of-life/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csharp_conways_game_of_life
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Cells count: ");
+            var cellsCount = int.Parse(Console.ReadLine());
+
+            var history = Universe.FromNothing(cellsCount).BigBang();
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class Universe
+    {
+        public static int HorizontalHorizont = 80;
+        public static int VerticalHorizont = 25;
+        public static TimeSpan GenerationLifespan = TimeSpan.FromMilliseconds(200);
+
+        public static HashSet<Tuple<int, int>> FromNothing(int count)
+        {
+            var random = new Random();
+            return new HashSet<Tuple<int, int>>(Enumerable.Range(0, count).Select(_ => Tuple.Create(random.Next(0, HorizontalHorizont), random.Next(0, VerticalHorizont))));
+        }
+
+        public static HashSet<Tuple<int, int>> Print(this HashSet<Tuple<int, int>> universe)
+        {
+            Console.Clear();
+            universe.ToList().ForEach(t =>
+                                          {
+                                              Console.SetCursorPosition(t.Item1, t.Item2);
+                                              Console.Write('■');
+                                          });
+            return universe;
+        }
+
+        public static HashSet<Tuple<int, int>> Evolve(this HashSet<Tuple<int, int>> universe)
+        {
+            var set = universe
+                .SelectMany(t => Enumerable.Range(0, 9).Select(i => Tuple.Create(t.Item1 + i / 3 - 1, t.Item2 + i % 3 - 1)))
+                .Distinct()
+                .Select(t => Tuple.Create(
+                                t.Item1,
+                                t.Item2,
+                                universe.Contains(t),
+                                universe.Count(c => Math.Abs(t.Item1 - c.Item1) <= 1 && Math.Abs(t.Item2 - c.Item2) <= 1) -
+                                (universe.Contains(t) ? 1 : 0)))
+                .Where(state => state.Item3
+                    ? state.Item4 == 2 || state.Item4 == 3
+                    : state.Item4 == 3)
+                .Where(state => state.Item1 >= 0 && state.Item2 >= 0 && state.Item1 < HorizontalHorizont && state.Item2 < VerticalHorizont)
+                .Select(state => Tuple.Create(state.Item1, state.Item2));
+            return new HashSet<Tuple<int, int>>(set);
+        }
+
+        public static async Task BigBang(this HashSet<Tuple<int, int>> universe)
+        {
+            await Task.Delay(GenerationLifespan);
+            await universe.Print().Evolve().BigBang();
+        }
+    }
+}
---
+++ fsharp-conways-game-of-life/Program.fs
@@ -0,0 +1,65 @@
+﻿(*
+Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+Any live cell with two or three live neighbours lives on to the next generation.
+Any live cell with more than three live neighbours dies, as if by overcrowding.
+Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+*)
+
+open System
+
+let VerticalBoundary = 25
+let HorizontalBoundary = 80
+
+type Universe = int * int list
+
+let UniverseWithGlider = [ (0, 1); (1, 2); (2, 0); (2, 1); (2, 2) ]
+let UniverseWithBlinker =  [ (2, 2); (2, 3); (2, 4) ]
+let UniverseWithBeacon = [0 .. 3] |> List.map (fun i -> i / 2, i % 2) |> List.map (fun (x, y) -> [ (x, y); (x + 2, y + 2) ]) |> List.concat
+let UniverseRandomLifeForm = 
+    Console.Write("Provide cells count and then hit any key to get a new generation: ")
+    let random = new Random()
+    let liveCellsCount = Console.ReadLine() |> Int32.Parse
+    [0 .. liveCellsCount] |> List.map (fun _ -> (random.Next(0, HorizontalBoundary), random.Next(0, VerticalBoundary))) |> Seq.distinct |> Seq.toList
+
+let output universe = 
+    Console.Clear()
+    let rec outputInternal cellslist =
+        match cellslist with
+        | [] -> ()
+        | head::tail -> 
+            let x,y = head
+            Console.SetCursorPosition(x, y)
+            Console.Write('■')
+            outputInternal tail
+    outputInternal universe
+
+let isCellInObservableUniverse (x,y) = x >= 0 && y >= 0 && x < HorizontalBoundary && y < VerticalBoundary
+
+let getAdjacentCells (x, y) = 
+    [0 .. 8] |> List.map (fun i -> (x + (i / 3) - 1, y + (i % 3) - 1))
+
+let evolve universe =
+    let allAdjacentCells = universe |> List.map getAdjacentCells |> List.concat |> Seq.distinct |> Seq.toList 
+    let produceCellState (a, b) =
+        let getAliveNeighbours (x:int, y:int) = (Math.Abs(a - x) <= 1 && Math.Abs(b - y) <= 1 && (a <> x || b <> y))
+        let countOfNeighbours = universe |> List.filter getAliveNeighbours |> List.length
+        let isCellAlive = universe |> List.filter (fun (x,y) -> x = a && y = b) |> List.length |> (fun l -> l > 0)
+        let state = (a, b, isCellAlive, countOfNeighbours)
+        state
+    let shouldCellLive (_,_,isCellAlive, countOfNeighbours) =
+        match isCellAlive, countOfNeighbours with
+        | true, n when n >= 2 && n <= 3 -> true
+        | false, n when n = 3 -> true
+        | _ -> false
+    let produceCell (x,y,_,_) = (x,y)
+
+    allAdjacentCells |> List.map produceCellState |> List.filter shouldCellLive |> List.map produceCell |> List.filter isCellInObservableUniverse
+    
+let rec live universe =
+    output universe
+    let readKey = Console.ReadKey()
+    match readKey.Key with
+    | ConsoleKey.Escape -> universe
+    | _ ->  universe |> evolve |> live
+
+UniverseRandomLifeForm |> live |> ignore
