8732f7caf0d4537ea6bed6875925e89489565417
Denis Postu <dpostu@kno.com>
2013-06-13 11:28:28 +0300

    Extracted a separate Universe class

--- csharp-conways-game-of-life/Program.cs
+++ csharp-conways-game-of-life/Program.cs
@@ -10,27 +10,53 @@ namespace csharp_conways_game_of_life
         private static void Main(string[] args)
         {
             Console.Write("Cells count: ");
-            var cellsCount = int.Parse(Console.ReadLine());
 
+            var cellsCount = int.Parse(Console.ReadLine());
             var history = Universe.FromNothing(cellsCount).BigBang();
 
             Console.ReadKey();
         }
     }
 
-    public static class Universe
+    public class Universe : HashSet<Tuple<int,int>>
     {
         public static int HorizontalHorizont = 80;
         public static int VerticalHorizont = 25;
-        public static TimeSpan GenerationLifespan = TimeSpan.FromMilliseconds(200);
 
-        public static HashSet<Tuple<int, int>> FromNothing(int count)
+        public Universe(IEnumerable<Tuple<int, int>> cells) : base(cells)
         {
-            var random = new Random();
-            return new HashSet<Tuple<int, int>>(Enumerable.Range(0, count).Select(_ => Tuple.Create(random.Next(0, HorizontalHorizont), random.Next(0, VerticalHorizont))));
         }
 
-        public static HashSet<Tuple<int, int>> Print(this HashSet<Tuple<int, int>> universe)
+        public static Universe FromNothing(int count)
+        {
+            var random = new Random();
+            return new Universe(Enumerable.Range(0, count).Select(_ => Tuple.Create(random.Next(0, HorizontalHorizont), random.Next(0, VerticalHorizont))));
+        }
+
+        public Universe Evolve()
+        {
+            var set = this
+                .SelectMany(t => Enumerable.Range(0, 9).Select(i => Tuple.Create(t.Item1 + i / 3 - 1, t.Item2 + i % 3 - 1)))
+                .Distinct()
+                .Select(t => Tuple.Create(
+                                t.Item1,
+                                t.Item2,
+                                this.Contains(t),
+                                this.Count(c => Math.Abs(t.Item1 - c.Item1) <= 1 && Math.Abs(t.Item2 - c.Item2) <= 1) - (this.Contains(t) ? 1 : 0)))
+                .Where(state => state.Item3
+                    ? state.Item4 == 2 || state.Item4 == 3
+                    : state.Item4 == 3)
+                .Where(state => state.Item1 >= 0 && state.Item2 >= 0 && state.Item1 < HorizontalHorizont && state.Item2 < VerticalHorizont)
+                .Select(state => Tuple.Create(state.Item1, state.Item2));
+            return new Universe(set);
+        }
+    }
+
+    public static class UniverseExtenstions
+    {
+        public static TimeSpan GenerationLifespan = TimeSpan.FromMilliseconds(200);
+        
+        public static Universe Print(this Universe universe)
         {
             Console.Clear();
             universe.ToList().ForEach(t =>
@@ -41,26 +67,7 @@ namespace csharp_conways_game_of_life
             return universe;
         }
 
-        public static HashSet<Tuple<int, int>> Evolve(this HashSet<Tuple<int, int>> universe)
-        {
-            var set = universe
-                .SelectMany(t => Enumerable.Range(0, 9).Select(i => Tuple.Create(t.Item1 + i / 3 - 1, t.Item2 + i % 3 - 1)))
-                .Distinct()
-                .Select(t => Tuple.Create(
-                                t.Item1,
-                                t.Item2,
-                                universe.Contains(t),
-                                universe.Count(c => Math.Abs(t.Item1 - c.Item1) <= 1 && Math.Abs(t.Item2 - c.Item2) <= 1) -
-                                (universe.Contains(t) ? 1 : 0)))
-                .Where(state => state.Item3
-                    ? state.Item4 == 2 || state.Item4 == 3
-                    : state.Item4 == 3)
-                .Where(state => state.Item1 >= 0 && state.Item2 >= 0 && state.Item1 < HorizontalHorizont && state.Item2 < VerticalHorizont)
-                .Select(state => Tuple.Create(state.Item1, state.Item2));
-            return new HashSet<Tuple<int, int>>(set);
-        }
-
-        public static async Task BigBang(this HashSet<Tuple<int, int>> universe)
+        public static async Task BigBang(this Universe universe)
         {
             await Task.Delay(GenerationLifespan);
             await universe.Print().Evolve().BigBang();
